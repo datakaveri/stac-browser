598431e12223ad688f1aae23cb963d2c7f8147b3
ojas <ojas24012002jain@gmail.com>
2025-04-16 16:40:34 +0530

    FIX: Tooltip error in console fix

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from "vue";
 import init from "./init";
 
+// Custom code : adding bootstrap to global
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+Vue.use(BootstrapVue); // Registers all BootstrapVue components and directives globally
 Vue.config.productionTip = false;
 
 init();
