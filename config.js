e1de60c32f643d99e6cf70b570582a7118da2b83
ojas <ojas24012002jain@gmail.com>
2025-04-16 16:12:37 +0530

    FIX: Config file rebased

--- config.js
+++ config.js
@@ -48,33 +48,5 @@ module.exports = {
     requestQueryParameters: {},
     socialSharing: ['email', 'bsky', 'mastodon', 'x'],
     preprocessSTAC: null,
-    // authConfig: {
-    //   type: 'apiKey',
-    //   in: 'header',
-    //   name: 'Authorization',
-    //   formatter: token => `Bearer ${token}`, // This is an example, there's also the simpler variant to just provide the string 'Bearer' in this case
-    //   description: `Please retrieve the token from our [API console](https://example.com/api-console).\n\nFor further questions contact <mailto:support@example.com>.`
-    // },
-    authConfig: {
-      type: "openIdConnect",
-      openIdConnectUrl:
-        "https://keycloak-update.iudx.io/auth/realms/demo/.well-known/openid-configuration",
-      oidcConfig: {
-        client_id: "angular-iudx-client",
-      },
-    },
-    dxConfig: {
-      dxUrl: "https://api.cat-test.iudx.io",
-      dxCatBasePath: "/iudx/cat/v1",
-    },
-    catalogUrl: "https://ogc.iudx.io/stac",
-  
-    allowedDomains: ["geoserver.dx.geospatial.org.in"],
-    // dxConfig: {
-    //   dxUrl: "https://dx.gsx.org.in",
-    //   dxCatBasePath: "/ugix/cat/v1",
-    // },
-    // catalogUrl: "https://geoserver.dx.gsx.org.in/stac",
-  
-    // allowedDomains: ["geoserver.dx.gsx.org.in"]
+    authConfig: null
 };
